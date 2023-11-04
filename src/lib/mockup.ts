0020bbd392e21e4b5de1e6f961ad635b381d27e7
Arman <37100541+CarboxyDev@users.noreply.github.com>
2023-11-05 02:12:38 +0530

    Fix deployment issue

--- src/lib/mockup.ts
+++ src/lib/mockup.ts
@@ -1,25 +1,23 @@
-import { Course } from '@/lib/types';
-
-export const mockCourses: Course[] = [
-  {
-    id: 'computer-networks-cs451-12f5l',
-    name: 'Computer Networks',
-    code: 'CSE232',
-    stream: 'CSE',
-    description:
-      'This course offers students a comprehensive exploration of modern computer networks. Students will gain an in-depth understanding of network architectures, protocols, security, wireless technologies, network management, and virtualization. The course equips students with the knowledge and skills required to proficiently design, configure, secure, and troubleshoot complex network environments.',
-    credits: 4,
-    prereqs: ['IP', 'DSA'],
-    professor: 'Dr. Vijay Kumar',
-    duration: 15,
-    timing: '10:00 - 11:30 AM (Mon, Fri)',
-    semester: 'Winter',
-    seats: 340,
-    openSeats: 70,
-    rating: 4.5,
-    ratingCount: 15,
-  },
-];
+// export const mockCourses: Course[] = [
+//   {
+//     id: 'computer-networks-cs451-12f5l',
+//     name: 'Computer Networks',
+//     code: 'CSE232',
+//     stream: 'CSE',
+//     description:
+//       'This course offers students a comprehensive exploration of modern computer networks. Students will gain an in-depth understanding of network architectures, protocols, security, wireless technologies, network management, and virtualization. The course equips students with the knowledge and skills required to proficiently design, configure, secure, and troubleshoot complex network environments.',
+//     credits: 4,
+//     prereqs: ['DSA'],
+//     professor: 'Dr. Vijay Kumar',
+//     duration: 15,
+//     timing: '10:00 - 11:30 AM (Mon, Fri)',
+//     semester: 'Winter',
+//     seats: 340,
+//     openSeats: 70,
+//     rating: 4.5,
+//     ratingCount: 15,
+//   },
+// ];
 
 interface MockReview {
   id: string;
