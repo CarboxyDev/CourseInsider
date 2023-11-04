dc8ea8512601aa4d2ab9121d44e4b9d9826e0bbd
Arman <37100541+CarboxyDev@users.noreply.github.com>
2023-11-04 20:58:44 +0530

    Add early version of profile page

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,11 @@
+import { PageWrapper } from '@/components/PageWrapper';
+
+export default function ProfilePage() {
+  return (
+    <PageWrapper>
+      <div className="mt-20 md:mt-60 flex items-center justify-center">
+        <h1 className="text-5xl font-semibold text-zinc-700">Coming Soon</h1>
+      </div>
+    </PageWrapper>
+  );
+}
